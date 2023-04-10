25f1dda812d7d45ebf5197e8dc48bfcfb9822afe
pattjoshi <omprakashpattjoshi123@gmail.com>
2023-04-11 02:23:28 +0530

    first commit

---
+++ src/components/CardsData.js
@@ -0,0 +1,138 @@
+const Cardsdata = [
+  {
+    id: 1,
+    rname: "VITS Hotel",
+    imgdata:
+      "https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1",
+    address: "North Indian, Biryani, Mughlai",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: " 1175 + Happy Customers",
+    price: 3150,
+    rating: "3.8",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 2,
+    rname: "NIRANTI",
+    imgdata:
+      "https://cf.bstatic.com/xdata/images/hotel/max1280x900/215955381.jpg?k=ff739d1d9e0c8e233f78ee3ced82743ef0355e925df8db7135d83b55a00ca07a&o=&hp=1",
+    address: "Street Food",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: " 2525 + Happy Customers",
+    price: 3325,
+    rating: "3.9",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 3,
+    rname: "TAJ HOTEL",
+    imgdata:
+      "https://cf.bstatic.com/xdata/images/hotel/max1280x900/322658536.jpg?k=3fffe63a365fd0ccdc59210188e55188cdb7448b9ec1ddb71b0843172138ec07&o=&hp=1",
+    address: "Pizza, Fast Food, Pasta",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: " 650 + Happy Customers",
+    price: 1270,
+    rating: "4.2",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 4,
+    rname: "Novetel Hotel",
+    imgdata:
+      "https://www.tajhotels.com/content/dam/luxury/tajbengal/16x7/66605233-H1-TBK_Poolside_13503__CMYK-16x7.jpg/jcr:content/renditions/cq5dam.web.1280.1280.jpeg",
+    address: "Momos",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: " 300 + Happy Customers",
+    price: 1760,
+    rating: "3.8",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 5,
+    rname: "TRIDENT HOTEL",
+    imgdata: "https://www.hotelmetdelhi.com/img/slideshow/banner1.jpg",
+    address: "North Indian",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: "1050 + Happy Customers",
+    price: 2610,
+    rating: "4.0",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 6,
+    rname: "Lemon Tree Hotel",
+    imgdata:
+      "https://img.etimg.com/thumb/width-1200,height-900,imgsize-20545,resizemode-1,msid-22427830/industry/services/hotels-/-restaurants/ministry-of-tourism-brings-out-new-minute-guidelines-for-star-luxury-hotels.jpg",
+    address: "Wraps FastFood, Chines",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: " 1100 + Happy Customers",
+    price: 1000,
+    rating: "3.8",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 7,
+    rname: "OORR Watervilla",
+    imgdata:
+      "https://curlytales.com/wp-content/uploads/2020/09/oorr-watervilla-bedroom-2-1170x731.jpg",
+    address: "North Indian, Fast Food",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: "500 + Happy Customers",
+    price: 3000,
+    rating: "3.8",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 8,
+    rname: "LELIA HOTEL",
+    imgdata:
+      "https://img.traveltriangle.com/blog/wp-content/uploads/2018/05/Moscow-Cover.jpg",
+    address: "Tea, Coffee, Shake, Beverages",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: "500 + Happy Customers",
+    price: 2000,
+    rating: "3.2",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 9,
+    rname: "ABODE HOTEL",
+    imgdata:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/91/0c/f2/seven-seas-hotel.jpg?w=300&h=300&s=1",
+    address: "Burger, Sandwich, Fast Food",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: "2525 + Happy Customers",
+    price: 1000,
+    rating: "3.8",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+];
+
+export default Cardsdata;
---
+++ src/components/HeroImg/hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './hero.css'
+import Featured from '../featured/Featured'
+
+
+const Hero = () => {
+    return (
+        <>
+            <div className='heroImage'>
+                <Featured />
+            </div>
+        </>
+    )
+}
+
+export default Hero;
